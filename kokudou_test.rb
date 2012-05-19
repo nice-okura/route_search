f804e8357e9fedeceee09dff5071ff870a01be81
fujitani <fujitani@agusa.i.is.nagoya-u.ac.jp>
2012-05-19 16:42:15 +0900

    GEOクラス作成

--- kokudou_test.rb
+++ kokudou_test.rb
@@ -29,13 +29,63 @@ PATH_B = "&output=xml&hl=ja&oe=UTF8"
 ROUTE_XPATH = "//addressline"
 
 #
-#=main
+#=緯度経度クラス
+#
+class GEO
+  attr_accessor :lat, :lon
+  def initialize(lat, lon)
+    @lat = lat
+    @lon = lon
+  end
+  
+  def get_n(offset=0.01) # 北側にずらして座標クラスを返す
+    return GEO.new(@lat+offset,@lon)
+  end
+  
+  def get_s(offset=0.01) # 南
+    return GEO.new(@lat-offset,@lon)
+  end
+  
+  def get_e(offset=0.01) # 東
+    return GEO.new(@lat,@lon+offset)
+  end
+  
+  def get_w(offset=0.01) # 西
+    return GEO.new(@lat,@lon-offset)
+  end
+  
+  def to_s
+    return "#{@lat},#{@lon}"
+  end
+end
+
+#
+#= get_addressline
+#== 緯度経度から道情報を返す
+#
+def get_addressline(geo)
+  url = "#{PATH_A}#{geo.to_s}#{PATH_B}"
+  doc = Nokogiri.HTML(open(url))
+  #return "#{doc}(#{geo.to_s})"
+  return "#{doc.xpath(ROUTE_XPATH).text}(#{geo.to_s})"
+end
+
+#
+#= main
 #
 lat = 36.260331
 lon = 137.439516
-lat.step(38.0, 0.001){ |l|
-  geo = "#{l},#{lon}"
-  url = "#{PATH_A}#{geo}#{PATH_B}"
-  doc = Nokogiri.HTML(open(url))
-  puts doc.xpath(ROUTE_XPATH).text
-}
+
+
+#lat.step(38.0, 0.001){ |l|
+g = GEO.new(lat, lon)
+road_name = get_addressline(g)
+puts road_name
+#while(1)
+  #puts get_addressline(g.get_n)
+  #puts get_addressline(g.get_e)
+  #puts get_addressline(g.get_w)
+  puts get_addressline(g.get_s)
+  
+#end
+#}
